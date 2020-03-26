f48ac21f890d639062130f34684a078f38b206db
Ed <edmilson.souza@outlook.com>
2020-03-26 23:07:29 +0200

    Added basic setup to capture a har file from Firefox

---
+++ src/test/java/org/ed/SimpleTest.java
@@ -0,0 +1,49 @@
+package org.ed;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.proxy.CaptureType;
+
+public class SimpleTest {
+    public static void main(String[] args) throws IOException {
+    	System.setProperty("webdriver.gecko.driver","/usr/local/Cellar/geckodriver/0.26.0/bin/geckodriver");
+    	
+    	
+    	DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+    	
+    	// --------- proxy setup ----------
+    	BrowserMobProxy proxy = new BrowserMobProxyServer();
+        proxy.start(0);
+        proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT );
+        proxy.newHar("google.com");
+        
+        Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+        capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
+        // ------------------------------
+    	
+    	@SuppressWarnings("deprecation")
+		WebDriver driver = new FirefoxDriver(capabilities);
+
+        driver.get("https://google.com");
+        Har har = proxy.getHar();
+        har.writeTo(new File("/Users/edsouza/Downloads/google.har"));
+
+        System.out.println(driver.getTitle());
+       
+        driver.close();
+       
+    }
+
+}
